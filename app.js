61cc80491b28eb1fd9654f3acdb8fbce1f4a0dca
Alix2044 <75444456+Alix2044@users.noreply.github.com>
2024-04-25 11:56:17 +0200

    added sessions

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const app = express();
 app.use(expressLayouts)
 app.set("view engine", "ejs")
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-
+// Sessions from Express to save sessions for login 
+  app.use(
+    session({
+      secret: 'SecretCode',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
 
 // Routes 
 
