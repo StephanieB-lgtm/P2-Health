0840f9e6763205903df991d049d9732e81d5a617
Alix2044 <75444456+Alix2044@users.noreply.github.com>
2024-04-26 08:52:13 +0200

    config.env

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use('/users',require('./routes/users'))
 
 const PORT =  process.env.PORT || 3000;
 
+
 app.listen( PORT, () => {
     console.log(`Server is running on port ${PORT} on http://localhost:${PORT}`);
 });
